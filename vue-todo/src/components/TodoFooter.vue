16ce45696d8a6c311b3258fd92e46d285264d8e3
myungjin\mjj0324 <mk910324>
2019-07-25 00:18:39 +0900

    05.template까지 실습

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -1,13 +1,20 @@
 <template>
-    <div>footer</div>
+    <div class="clearAllContainer">
+        <button class="clearAllBtn" @click="clearTodo">Clear All</button>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "TodoFooter.vue"
+        methods: {
+            clearTodo() {
+                localStorage.clear();
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .clearAllContainer {text-align:center;}
+    .clearAllBtn {width:8.5rem; height:50px; color:#e20303; line-height:50px; background:#fff; border:0; cursor:pointer;}
 </style>
--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,34 +1,37 @@
 <template>
-  <div>
-      <div class="inputBox shadow">
-        <input type="text" v-model="newTodoItem" v-on:keyup.enter = "addTodo" placeholder="Type what you have to do">
-        <button v-on:click="addTodo">추가</button>
-      </div>
+  <div class="inputBox shadow">
+      <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo" placeholder="Type what you have to do">
+    <button v-on:click="addTodo" class="addContainer">
+        <i class="addBtn fas fa-plus" aria-hidden="true"></i>
+    </button>
   </div>
 </template>
 
 <script>
-    export default {
-        data() {
-          return {
-            newTodoItem: ''
-          }
-        },
-      methods: {
-          addTodo() {
-            if(!this.newTodoItem) {
-              var value = this.newTodoItem && this.newTodoItem.trim();
-              localStorage.setItem(value, value);
-              this.clearInput();
-            }
-          },
-        clearInput() {
-            this.newTodoItem = '';
-        }
-      }
-    }
+   export default {
+         data() {
+               return {
+                 newTodoItem: ''
+               }
+         },
+         methods: {
+               addTodo() {
+                  if(this.newTodoItem !== '') {
+                      var value  = this.newTodoItem && this.newTodoItem.trim();
+                       localStorage.setItem(value, value);
+                       this.clearInput();
+                   }
+               },
+             clearInput() {
+                   this.newTodoItem = '';
+             }
+       }
+   }
 </script>
 
 <style scoped>
-
+    .inputBox {overflow:hidden; background:#fff; border-radius:5px;}
+    .inputBox input {box-sizing:border-box; width:calc(100% - 3rem); height:50px; padding:0 20px;  font-size:0.9rem; line-height:50px;border-style:none;}
+    .addContainer {display:block; float:right; width:3rem; height:50px; padding:0; background:linear-gradient(to right, #6478FB, #8763FB) ; border:0; border-radius:0 5px 5px 0; cursor:pointer;}
+    .addBtn {color:#fff; vertical-align:middle;}
 </style>
